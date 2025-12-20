2340d29cf332c1102a6b2cdf5460778bb65e4dba
user <kosyanip@gmail.com>
2025-12-20 05:26:30 +0300

    learning

--- constants.py
+++ constants.py
@@ -21,13 +21,12 @@ PIECE_VALUES = {
 }
 
 SAVE_MODEL_PER_GAMES = 100
-TRAINING_MCTS_SIMULATIONS=128
-TRAINING_MAX_MOVES=400
+TRAINING_MCTS_SIMULATIONS=256
+TRAINING_MAX_MOVES=100_000
 INFERENCE_BATCH_SIZE = 256
 DIRICHLET_ALPHA = 0.3
 DIRICHLET_EPSILON = 0.25
-CONTEMPT_AGAINST_DRAW = -0.1
-REPETITION_PENALTY = -1.0
+CONTEMPT_AGAINST_DRAW = -0.5
 THREEFOLD = False
 
 LEARNING_RATE = 1e-4
@@ -35,17 +34,17 @@ WEIGHT_DECAY = 1e-5
 GRADIENT_CLIP_NORM = 1.0
 
 # temperature schedule (self-play)
-TEMPERATURE_MOVES = 60
-TEMPERATURE_TAU_START = 1.2     # tau на самом старте партии
-TEMPERATURE_TAU_END   = 0.2    # минимальный tau перед переходом в greedy
-TEMPERATURE_DECAY_PLY = 60     # за сколько полуходов (ply) опускаем tau до end
+TEMPERATURE_MOVES = 448
+TEMPERATURE_TAU_START = 3.0     # tau на самом старте партии
+TEMPERATURE_TAU_END   = 0.15    # минимальный tau перед переходом в greedy
+TEMPERATURE_DECAY_PLY = 384     # за сколько полуходов (ply) опускаем tau до end
 
 
 # ================== Гиперпараметры буфера ==================
-REPLAY_CAPACITY = 100_000       # максимум позиций в буфере
+REPLAY_CAPACITY = 300_000       # максимум позиций в буфере
 MIN_REPLAY_SIZE = 30_000        # с какого размера буфера начинаем full-обучение
 BATCH_SIZE = 256               # размер минибатча
-TRAIN_STEPS_PER_ITER = 4      # сколько SGD-шагов на одну итерацию
+TRAIN_STEPS_PER_ITER = 2      # сколько SGD-шагов на одну итерацию
 DEFAULT_REPLAY_PATH = "replay_buffer/replay_buffer.npz"
 SAVE_BUFFER_PER_GAMES = 500
 # ===========================================================
