63643841e29d4ac3d7dd28f6f42886dc71005494
Kirill <kosyanip@gmail.com>
2025-12-28 23:53:15 +0300

    removed redundant files

--- random_agent.py
+++
@@ -1,9 +0,0 @@
-from random import choice
-
-class RAgent:
-    def __init__(self, board):
-        self.board = board
-
-    def make_move(self, obs=None, legal_mask=None):
-        moves = list(self.board.legal_moves)
-        return choice(moves)
